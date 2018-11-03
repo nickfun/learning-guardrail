2be55fe6fb3fd776a65ffb44ae6133cfd0e955e4
Nick F <nick@nick.gs>
2018-11-02 22:21:20 -0700

    only readme needed on gh pages branch

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JAVA_TOOL_OPTIONS -jar target/todoapi-1.0-SNAPSHOT-fatjar.jar
--- clean-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PORT=8080
-export DOMAIN=http://localhost:8080
-mvn clean
-mvn package && java -jar target/todoapi-1.0-SNAPSHOT-fatjar.jar
